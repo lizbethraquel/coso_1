8d09c263bb9901c4ada9d7ca4e1e804da5e1f6a4
Lizbeth Bernal <lizbercas@alu.edu.gva.es>
2023-05-10 10:09:43 +0200

    primer commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>
+        hola que tal me llamo lizbeth
+    </h1>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1> mi home </h1>
+  <lizbeth/>
+</template>
+
+<script setup>
+  import lizbeth from '../components/Nav.vue'
+</script>
