97a6a81db4976d73ce78e99f60282a3be1919248
Lizbeth Bernal <lizbercas@alu.edu.gva.es>
2023-05-31 09:16:09 +0200

    erro

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.Node_ENV === "production" ? " /coso_1/" : "/",
+  base: process.env.Node_ENV === "production" ? "/coso_1/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
